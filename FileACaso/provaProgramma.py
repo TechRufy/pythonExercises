92c7b63ba04ebc7048f973fd47d3dc632690d747
TechRufy <alb.genovese@gmail.com>
2022-09-23 16:39:04 +0200

    url

--- FileACaso/provaProgramma.py
+++ FileACaso/provaProgramma.py
@@ -1,12 +1,1 @@
 
-from xml.etree.ElementInclude import include
-
-
-import proveDjango.FileACaso.provaClasse as provaClasse
-
-y = provaClasse.Utente("carlo","lucarelli",1234567)
-
-print(y)
-
-if __name__ == "__main__":
-    print("ciao")
