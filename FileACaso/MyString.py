d7fd01807e2d2691f14ccdde8041b9db7bb99231
TechRufy <alb.genovese@gmail.com>
2022-10-23 23:02:08 +0200

    esercizi del 23/10/2022

---
+++ FileACaso/MyString.py
@@ -0,0 +1,11 @@
+class MyString(str):
+
+    def __new__(cls, *args, **kwargs):
+        obj = super().__new__(cls, *args, **kwargs)
+        obj = obj.upper()
+        return obj
+
+
+c = MyString("cia342o")
+
+print(c)
