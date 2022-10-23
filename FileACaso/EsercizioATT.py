2d21fa90968176a943ce61202a9689e88522532d
TechRufy <alb.genovese@gmail.com>
2022-10-23 22:53:53 +0200

    esericizi del 23/10/2022

---
+++ FileACaso/EsercizioATT.py
@@ -0,0 +1,17 @@
+"""
+Scrivere una classe C per cui accade che ogni volta che si aggiunge una variabile d'istanza a una delle istanze di C
+in realtà la variabile viene aggiunta alla classe come variabile di classe.
+"""
+
+
+class C:
+
+    def __setattr__(self, key, value):
+        return setattr(C, key, value)
+
+
+c = C()
+
+c.variabileClasse = 100
+
+print(C.variabileClasse)
